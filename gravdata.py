f8e1b0faf8c9655df2d53a2c9745fb493fd5a849
Felix Widmann <widmann.f@gmx.de>
2021-09-16 17:16:43 +0200

    Removing getIntdata from gravdata init

--- gravdata.py
+++ gravdata.py
@@ -144,7 +144,6 @@ class GravData():
                 if not datacatg == 'ASTROREDUCED':
                     self.wlFT = fits.open(self.name)['OI_WAVELENGTH', 20].data['EFF_WAVE']*1e6
         
-        self.getIntdata(plot=False, flag=False)
     
     def getValue(self, ext1, ext2=None, ext1num=None):
         """
