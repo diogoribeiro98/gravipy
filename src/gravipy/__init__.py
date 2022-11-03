c024ae2f84ebae20709838270d38248058423e43
Felix Widmann <widmann.f@gmx.de>
2022-11-03 11:58:37 +0100

    modelling functions removed

--- src/gravipy/__init__.py
+++ src/gravipy/__init__.py
@@ -1,8 +1,6 @@
-from .gravmodel import *
 from .gravdata import *
 from .gravfit import *
 from .gravmfit import *
 from .phasecor import *
 from .gcobs import *
 from .gcorbits import *
-# from .star_orbits import *
