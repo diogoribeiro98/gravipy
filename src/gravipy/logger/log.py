a957268ee23e24cdd8c15c63e83b9a67b310c7bb
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-01-29 09:16:06 +0100

    solved naming conflict for logging package

--- src/gravipy/logger/log.py
+++ src/gravipy/logger/log.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import logging.config as logging_cf
 from pkg_resources import resource_filename
 
 log_level_mapping = {
@@ -13,10 +14,10 @@ log_level_mapping = {
 def setup_logging():
     """Setup logging format and handlers according to the json file
     """
-    config_file = resource_filename(__name__, 'config.json')
+    config_file = resource_filename(__name__, './config_file.json')
     
     with open(config_file) as f_in:
         config = json.load(f_in)
 
-    logging.config.dictConfig(config)
+    logging_cf.dictConfig(config)
 
