a225654c26e9873b07270e666e5a2fd9b2bbeec8
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2025-04-15 10:04:38 +0200

    direct import of plot_interferometric_data

--- src/gravipy/data/__init__.py
+++ src/gravipy/data/__init__.py
@@ -1,3 +1,5 @@
 from .interferometric_data_class import InterferometricData
 from .scivis import GraviData_scivis
 from .raw import GraviData_raw
+
+from .plot_idata import plot_interferometric_data
